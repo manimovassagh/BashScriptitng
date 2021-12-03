dc42bb40ce62cc689a5e150fb28ea9bcde6f658b
manimovassagh <mani.mgh@gmail.com>
2021-12-03 12:22:42 +0100

    Powershell

--- myscript.sh
+++ myscript.sh
@@ -6,10 +6,10 @@ echo Hello World!!
 #VARIABLES
 #Uppercase by Convention
 #Letters , numbers, underscores
-NAME="Mani"
-echo "My name is $NAME"
-echo "My name is ${NAME}"
+# NAME="Mani"
+# echo "My name is $NAME"
+# echo "My name is ${NAME}"
 #User Input
-read -p "Enter Your Name " NAME
-echo "Hello $NAME , Nice To meet you"
-if []
+# read -p "Enter Your Name " NAME
+# echo "Hello $NAME , Nice To meet you"
+C:\Users
---
+++ script.ps1
@@ -0,0 +1,1 @@
+Set-Location C:\Users\mani.movassaghghazan\Desktop
