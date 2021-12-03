0197786506cb3dc7668cfdc23ae2caa5ee9578c7
manimovassagh <mani.mgh@gmail.com>
2021-12-03 11:43:16 +0100

    bash

---
+++ myscript.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#ECHO COMMAND
+echo Hello World!!
+
+#VARIABLES
+#Uppercase by Convention
+#Letters , numbers, underscores
+NAME="Mani"
+echo "My name is $NAME"
+echo "My name is ${NAME}"
